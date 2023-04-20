fa94b3a5f1a46853cfe1c6981c952adcd8f3b0df
alexander <castro.t.alex@gmail.com>
2023-04-20 15:03:00 -0500

    Add className instead class

--- artaich/components/dalle/dalle.js
+++ artaich/components/dalle/dalle.js
@@ -70,7 +70,7 @@ export default function DalleIA() {
         <img
           src="https://oaidalleapiprodscus.blob.core.windows.net/private/org-hKo4zatnLQOGFJyFSCL9LhU5/user-siiYEBCRytDaLBAH419XYo6s/img-FJfPO29iFhOwDlSqiyNv3J5t.png?st=2023-04-06T00%3A23%3A57Z&se=2023-04-06T02%3A23%3A57Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-04-05T20%3A50%3A03Z&ske=2023-04-06T20%3A50%3A03Z&sks=b&skv=2021-08-06&sig=tyuoEK436HL/4rI6KYbTlWO/jRzmT2hRTG/DalgDkW8%3D"
           alt="Your image"
-          class="w-48 h-48 object-cover"
+          className="w-48 h-48 object-cover"
         />
         <Menu as="div" className="relative inline-block text-left">
           <div>
