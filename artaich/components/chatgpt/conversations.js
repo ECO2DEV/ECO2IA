22eb6ec0f67ab6600da2c2087b6b2be0f0bd0985
alexander <castro.t.alex@gmail.com>
2023-05-10 16:32:47 -0500

    Adding isLoading to the conversation chatGpt

--- artaich/components/chatgpt/conversations.js
+++ artaich/components/chatgpt/conversations.js
@@ -8,7 +8,7 @@ const strapiUrl = process.env.STRAPI_URL;
 export const Conversations = () => {
   const { user } = useContext(UserContext);
 
-  const { data } = useChat(user?.id);
+  const { data, isLoading } = useChat(user?.id);
   // console.log('Request of chatGpt user, bot', data);
   const reversedData = data?.data?.slice().reverse();
 
@@ -20,13 +20,29 @@ export const Conversations = () => {
     }
   }, [data]);
 
+  if (isLoading) {
+    return (
+      <div className="border border-blue-300 shadow p-4 w-full">
+        <div className="animate-pulse flex space-x-4">
+          <div className="rounded-full bg-slate-700 h-10 w-10"></div>
+          <div className="flex-1 space-y-6 py-1">
+            <div className="h-2 bg-slate-700 rounded"></div>
+            <div className="space-y-3">
+              <div className="grid grid-cols-3 gap-4">
+                <div className="h-2 bg-slate-700 rounded col-span-2"></div>
+                <div className="h-2 bg-slate-700 rounded col-span-1"></div>
+              </div>
+              <div className="h-2 bg-slate-700 rounded"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="h-full bg-gray-100">
       <section className="flex flex-col text-sm h-[80vh] lg:h-[85vh] overflow-y-scroll overflow-x-hidden">
-        {/* {
-          // create a skeleton loading effect
-          isLoading && <div className="animate-pulse">Loading...</div>
-        } */}
         {reversedData?.map((item) => (
           <div key={item.id}>
             <div className={`group w-full text-gray-800 bg-gray-100 `}>
