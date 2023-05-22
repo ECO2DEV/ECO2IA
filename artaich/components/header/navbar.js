f06a69a522e40b8b3f3eba9cf7e2199a6d5ca02c
Davidricardocristan <127789117+Davidricardocristan@users.noreply.github.com>
2023-05-21 20:39:35 -0500

    Fix Conflicts

--- artaich/components/header/navbar.js
+++ artaich/components/header/navbar.js
@@ -170,7 +170,7 @@ export default function Header({router}) {
                   {DataNavbar.menu_opt_2}
                 </Link>
                 <Link
-                  href="/home"
+                  href="/"
                   className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   {DataNavbar.menu_opt_3}
