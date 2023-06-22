589d14a3b8325d04466a0dbafb31b0a29328ba8a
alexander <castro.t.alex@gmail.com>
2023-06-21 22:15:33 -0500

    Adding the count tokens to sidebar

--- artaich/util/helpers/count_tokens.js
+++ artaich/util/helpers/count_tokens.js
@@ -1,18 +1,19 @@
-const {encode, decode} = require('@nem035/gpt-3-encoder');
+const { encode, decode } = require('@nem035/gpt-3-encoder');
 /**
- * 
- * @param {words promp + response} text 
- * @returns 
+ *
+ * @param {words promp + response} text
+ * @returns
  */
-export default function countTokens(text) {
-    // Use a regular expression to split the text into an array of words
-    let words = encode(text)
-    
-    // Return the length of the words array
- 
-    return words ? words.length : 0;
+export function countTokens(text) {
+  // Use a regular expression to split the text into an array of words
+  // if text is an object, convert it to a string
+  // console.log('type of text', typeof text);
+  // console.log(text);
 
+  if (typeof text === 'object' || typeof text === 'array') {
+    text = JSON.stringify(text);
   }
 
-
-  
+  let words = encode(text);
+  return words ? words.length : 0;
+}
