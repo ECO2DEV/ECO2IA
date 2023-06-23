49621402f74498f720e720ee663489a93e672364
Code-Nature <codenature01001@gmail.com>
2023-06-22 20:53:21 -0500

    PDF / correo / whatsaap Share functionality

--- artaich/components/sport_coach/SportCoach.js
+++ artaich/components/sport_coach/SportCoach.js
@@ -5,6 +5,8 @@ import { sendTrainingPlanRequest } from "../../util/api/sendTrainingPlanRequest"
 import { SportCoachResults } from "./SportCoachResults";
 import { useSportCoach } from "../../hooks/useSportCoach";
 import { WelcomeSport } from "./welcomesport";
+import { DataMatTraining } from "../../data/mattraining";
+
 export const SportCoachIA = (props) => {
   // Estados para almacenar los datos del formulario
   const [weight, setWeight] = useState("");
@@ -14,7 +16,7 @@ export const SportCoachIA = (props) => {
   const [submitting, setSubmitting] = useState(false);
   const [error, setError] = useState("");
   const [showResults, setShowResults] = useState(false);
-  // Obtener el usuario de las props
+
   const user = props.user;
 
   // Obtener los datos del entrenador deportivo personalizado
@@ -42,7 +44,7 @@ export const SportCoachIA = (props) => {
         .then((response) => {
           setResponse(response?.data?.data);
           mutate({ data: [...data.data, response?.data], ...data });
-          setShowResults(true); // Mostrar los resultados generados
+          setShowResults(true);
         })
         .catch((error) => {
           setError(
@@ -78,86 +80,20 @@ export const SportCoachIA = (props) => {
   // Manejar el envío del formulario
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setShowResults(false); // Ocultar los resultados generados
+    setShowResults(false);
     fetchData();
   };
 
   return (
-    <div className="flex flex-col items-center w-fullmin-h-screen mt-0">
-     {!showResults && <WelcomeSport className="mb-8" />}
-    <form onSubmit={handleSubmit} className="grid grid-cols-4 gap-4 w-full">
-  <div className="col-span-1">
-    <fieldset className="my-10">
-      <InputField
-        label="Poids"
-        name="weight"
-        value={weight}
-        onChange={(e) => setWeight(e.target.value)}
-        type="text"
-      />
-    </fieldset>
-  </div>
-  <div className="col-span-1">
-    <fieldset className="my-10">
-      <InputField
-        label="Âge"
-        name="age"
-        value={age}
-        onChange={(e) => setAge(e.target.value)}
-        type="text"
-      />
-    </fieldset>
-  </div>
-  <div className="col-span-1 my-10">
-    <label htmlFor="goal" className="block text-sm font-medium text-gray-700 ">
-    Objectif
-    </label>
-    <select
-      id="goal"
-      name="goal"
-      value={goal}
-      onChange={handleChange}
-      className="mt-1 px-4 py-2 border border-gray-300 rounded-md w-full focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-    >
-      <option value="weight loss">Perte de poids</option>
-      <option value="muscle building">Renforcement musculaire</option>
-      <option value="mass">Prise de masse</option>
-      <option value="crossfit">Crossfit</option>
-      <option value="dry">Sèche</option>
-    </select>
-  </div>
-  <div className="col-span-1 my-10">
-    <label htmlFor="trainingDays" className="block text-sm font-medium text-gray-700">
-      Nombre d'entraînements par semaine
-    </label>
-    <select
-      id="trainingDays"
-      name="trainingDays"
-      value={trainingDays}
-      onChange={handleChange}
-      className="mt-1 px-4 py-2 border border-gray-300 rounded-md w-full focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-    >
-      <option value="1">1 jour</option>
-      <option value="2">2 jours</option>
-      <option value="3">3 jours</option>
-      <option value="4">4 jours</option>
-      <option value="5">5 jours</option>
-      <option value="6">6 jours</option>
-      <option value="7">7 jours</option>
-    </select>
-  </div>
-  <div className="col-span-4 flex mt-0 justify-center">
-    <button
-      type="submit"
-      disabled={submitting}
-      className="inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-    >
-      {submitting ? "Chargement..." : "Obtenir un plan d'entraînement"}
-    </button>
-  </div>
-</form>
-      {showResults && (
-        <div className="flex justify-center">
+    <div className="flex flex-col items-center w-full min-h-screen mt-0">
+      <h1 className="text-2xl font-bold text-center mb-8">
+        {DataMatTraining.Title}
+      </h1>
+      <p>{DataMatTraining.SubTitle}</p>
+      {!showResults ? (
+        <WelcomeSport className="mb-8" />
+      ) : (
+        <div className="flex justify-center mt-8">
           <div className="p-4 sm:p-8 md:mt-8 lg:max-w-xl xl:max-w-2xl">
             <SportCoachResults
               weight={weight}
@@ -168,6 +104,88 @@ export const SportCoachIA = (props) => {
           </div>
         </div>
       )}
+      <form
+        onSubmit={handleSubmit}
+        className="max-w-4xl w-full mx-auto px-4 sm:px-6 lg:px-8"
+      >
+        <div className="flex sm:grid-cols-2 gap-4">
+          <div>
+            <fieldset className="my-4">
+              <InputField
+                label="Poids"
+                name="weight"
+                value={weight}
+                onChange={(e) => setWeight(e.target.value)}
+                type="text"
+              />
+            </fieldset>
+          </div>
+          <div>
+            <fieldset className="my-4">
+              <InputField
+                label="Âge"
+                name="age"
+                value={age}
+                onChange={(e) => setAge(e.target.value)}
+                type="text"
+              />
+            </fieldset>
+          </div>
+          <div className="my-4">
+            <label
+              htmlFor="goal"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Objectif
+            </label>
+            <select
+              id="goal"
+              name="goal"
+              value={goal}
+              onChange={handleChange}
+              className="mt-1 px-4 py-2 border border-gray-300 rounded-md w-full focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            >
+              <option value="weight loss">Perte de poids</option>
+              <option value="muscle building">Renforcement musculaire</option>
+              <option value="mass">Prise de masse</option>
+              <option value="crossfit">Crossfit</option>
+              <option value="dry">Sèche</option>
+            </select>
+          </div>
+          <div className="my-4">
+            <label
+              htmlFor="trainingDays"
+              className="block text-sm font-medium text-gray-700"
+            >
+              N° d'entraînements
+            </label>
+            <select
+              id="trainingDays"
+              name="trainingDays"
+              value={trainingDays}
+              onChange={handleChange}
+              className="mt-1 px-4 py-2 border border-gray-300 rounded-md w-full focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            >
+              <option value="1">1 jour</option>
+              <option value="2">2 jours</option>
+              <option value="3">3 jours</option>
+              <option value="4">4 jours</option>
+              <option value="5">5 jours</option>
+              <option value="6">6 jours</option>
+              <option value="7">7 jours</option>
+            </select>
+          </div>
+        </div>
+        <div className="flex mt-8 justify-center">
+          <button
+            type="submit"
+            disabled={submitting}
+            className="inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            {submitting ? "Chargement..." : "Obtenir un plan d'entraînement"}
+          </button>
+        </div>
+      </form>
       {error && <h4 className="text-red-500 text-center">{error}</h4>}
     </div>
   );
