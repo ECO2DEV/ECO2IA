4c2fee883f618c33e51cbdea246090d3429cc253
alexander <castro.t.alex@gmail.com>
2023-06-28 22:05:57 -0500

    Adding next-share library

--- artaich/components/icons/icons.js
+++ artaich/components/icons/icons.js
@@ -338,7 +338,7 @@ export function ShowAnswerIcon() {
     </svg>
   );
 }
-export function FacebookIcon() {
+export function FacebookIconSVG() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -352,7 +352,7 @@ export function FacebookIcon() {
   );
 }
 
-export function TwitterIcon() {
+export function TwitterIconSVG() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -366,7 +366,7 @@ export function TwitterIcon() {
   );
 }
 
-export function TelegramIcon() {
+export function TelegramIconSVG() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -380,7 +380,7 @@ export function TelegramIcon() {
   );
 }
 
-export function WhatsAppIcon() {
+export function WhatsAppIconSVG() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -394,7 +394,7 @@ export function WhatsAppIcon() {
   );
 }
 
-export function InstagramIcon() {
+export function InstagramIconSVG() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -426,6 +426,25 @@ export function DefaultSocialIcon() {
       <path d="M3.6 15h16.8"></path>
       <path d="M11.5 3a17 17 0 0 0 0 18"></path>
       <path d="M12.5 3a17 17 0 0 1 0 18"></path>
+    </svg>
+  );
+}
+
+export function GlobalShareIcon() {
+  return (
+    <svg
+      className="w-7 h-7 text-gray-900  rounded-full  bg-gray-200 hover:bg-indigo-600 hover:-scale-110 transition-colors duration-200 p-1"
+      stroke="currentColor"
+      strokeWidth={1.5}
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z"
+      />
     </svg>
   );
 }
