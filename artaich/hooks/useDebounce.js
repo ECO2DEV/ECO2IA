2daf744129959a26882151dfde595276153f852a
alexander <castro.t.alex@gmail.com>
2023-07-31 10:03:25 -0500

    Create mobilepopup, the toggle button, usedebounce

--- artaich/hooks/useDebounce.js
+++ artaich/hooks/useDebounce.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
 
-export function useDebounce(value, delay = 500) {
+export function useDebounce(value, delay = 1000) {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setDebouncedValue(value);
+      if (value !== undefined) {
+        setDebouncedValue(value);
+      }
     }, delay);
 
     return () => {
