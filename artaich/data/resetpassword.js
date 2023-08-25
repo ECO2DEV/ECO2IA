1776bca11853fbc7c0787a687fe169ee9425feb3
David Ricardo Cristancho <davidricardocristan@gmail.com>
2023-08-25 11:19:51 -0500

    Fix conflict associated with ResetPassword UI

--- artaich/data/resetpassword.js
+++ artaich/data/resetpassword.js
@@ -4,5 +4,6 @@ export const DataResetPassword = {
     ResetPasswordTitle:"Réinitialiser le mot de passe",
     NewPassword:"Nouveau mot de passe",
     ResentPassword:"Renvoyer le mot de passe",
+    PasswordMust:"Le mot de passe doit comporter au moins 6 caractères.",
     Instructions:"Veuillez choisir un mot de passe sécurisé et unique qui combine des lettres (majuscules et minuscules), des chiffres et des symboles."
 }
