865f0e31f3396fdabbec66ea473592be35193ae2
alexander <castro.t.alex@gmail.com>
2024-01-26 18:25:13 -0500

    Adding the maticon and title in every ai

--- artaich/util/helpers/count_tokens.js
+++ artaich/util/helpers/count_tokens.js
@@ -9,18 +9,17 @@ export function countTokens(text, isGPT4Model = false) {
   // if text is an object, convert it to a string
   // console.log('type of text', typeof text);
   // console.log(text);
+  let words = 0;
 
   if (typeof text === 'object' || typeof text === 'array') {
     text = JSON.stringify(text);
   }
 
   if (isGPT4Model) {
-    return text ? text.length * 4 : 0;
+    words = encode(text);
+    return words ? words.length * 4 : 0; // Multiplica por 4 si es modelo GPT-4
+  } else {
+    words = encode(text);
+    return words ? words.length : 0;
   }
-  // console.log('text', text);
-  let words = encode(text);
-  // if it is a GPT-4 model, multiply the tokens by 4
-
-  // console.log('words', words);
-  return words ? words.length : 0;
 }
